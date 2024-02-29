1053d0f8f9b477effd02514e6a3b49ede9fa1d00
Sean <fengying1028@hotmail.com>
2024-02-28 19:48:59 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <Person/>
+</template>
+<script setup lang="ts" name="App">
+  import Person from "@/components/Person.vue";
+</script>
+<style scoped></style>
---
+++ src/components/Person.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>hello</h1>
+    </div>
+</template>
+
+<script lang="ts" setup name="Person">
+    import { type PersonInterface, type Persons } from '@/types';
+    let person:PersonInterface = {
+        id:`asfsda01`,
+        name:'zs',
+        age:18
+    }
+
+    let personList:Persons = [
+        {id: `safsdf01`, name:'zs',age: 30},
+        {id: `safsdf02`, name:'ls',age: 12},
+        {id: `safsdf03`, name:'ww',age: 51}
+    ]
+</script>
+
+<style scoped>
+
+</style>
