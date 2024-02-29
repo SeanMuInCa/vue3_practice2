dc6443e947d4683f2b4f4d81ff1004a1e8e7b592
Sean <fengying1028@hotmail.com>
2024-02-28 20:08:27 -0600

    props1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <Person/>
+  <Person personList/>
 </template>
 <script setup lang="ts" name="App">
   import Person from "@/components/Person.vue";
+  import { type PersonInterface, type Persons } from '@/types';
+  import {reactive} from 'vue'
+
+
+
+  let personList = reactive<Persons>([//泛型是最佳实践
+        {id: `safsdf01`, name:'zs',age: 30},
+        {id: `safsdf02`, name:'ls',age: 12},
+        {id: `safsdf03`, name:'ww',age: 51}
+    ]);
 </script>
 <style scoped></style>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,17 +1,22 @@
 <template>
     <div>
-        <h1>hello</h1>
+        <ul>
+            <li v-for="item in personList" :key="item.id">{{ item.name }} --- {{ item.age }}</li>
+        </ul>
     </div>
 </template>
 
-<script lang="ts" setup name="Person">
+<script lang="ts" setup name="Person" :props="props">
+
+
+    import { defineProps } from 'vue';
     import { type PersonInterface, type Persons } from '@/types';
     let person:PersonInterface = {
         id:`asfsda01`,
         name:'zs',
         age:18
     }
-
+    defineProps(['personList']);
     let personList:Persons = [
         {id: `safsdf01`, name:'zs',age: 30},
         {id: `safsdf02`, name:'ls',age: 12},
