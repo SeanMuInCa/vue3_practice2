71d2e7826ed9d7eaadb5c81a71440c0821d60fc1
Sean <fengying1028@hotmail.com>
2024-02-28 21:03:35 -0600

    props3

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -9,19 +9,25 @@
 <script lang="ts" setup name="Person" :props="props">
 
 
-    import { defineProps } from 'vue';
-    import { type PersonInterface, type Persons } from '@/types';
+    import { withDefaults } from 'vue';
+    import { type PersonInterface, type Persons } from '@/types/index';
     let person:PersonInterface = {
         id:`asfsda01`,
         name:'zs',
         age:18
     }
-    defineProps<{personList:Persons}>();
-    let personList:Persons = [
-        {id: `safsdf01`, name:'zs',age: 30},
-        {id: `safsdf02`, name:'ls',age: 12},
-        {id: `safsdf03`, name:'ww',age: 51}
-    ]
+
+    let list:Persons = [
+        {id: `safsdf01`, name:'zs1',age: 30},
+        {id: `safsdf02`, name:'ls2',age: 12},
+        {id: `safsdf03`, name:'ww3',age: 51}
+    ];
+
+
+    withDefaults(defineProps<{personList?:Persons}>(),{
+        personList:() => [{id:'adfa',name:'adfa',age:1}]//默认值
+    });
+    
 </script>
 
 <style scoped>
