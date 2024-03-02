796b4028075e1fb5927e5d0be15cec597a0f858d
Sean <fengying1028@hotmail.com>
2024-03-02 13:15:18 -0600

    路由传参

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -3,18 +3,22 @@
         <!-- <li>{{ route.query.id }}</li>
         <li>{{ route.query.title }}</li>
         <li>{{ route.query.content }}</li> -->
-        <li>{{ route.params.id }}</li>
+        <!-- <li>{{ route.params.id }}</li>
         <li>{{ route.params.title }}</li>
-        <li>{{ route.params.content }}</li>
+        <li>{{ route.params.content }}</li> -->
+        <li>{{ id }}</li>
+        <li>{{ title }}</li>
+        <li>{{ content }}</li>
 
     </ul>
 </template>
 
 <script lang="ts" setup name="Detail">
-import {useRoute} from 'vue-router';
-const route = useRoute();
-console.log(route);
+// import {useRoute} from 'vue-router';
+// const route = useRoute();
+// console.log(route);
 
+defineProps(['id','title','content'])//配置了路由传参后，就可以都当成props来处理
 </script>
 
 <style scoped>
