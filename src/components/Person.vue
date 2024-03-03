3bbb8fd3466769971b7b2208fa7c2251fd9d442b
Sean <fengying1028@hotmail.com>
2024-03-03 11:25:38 -0600

    不要用torefs

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -16,6 +16,7 @@
     <h1>{{ countStore.count }}</h1>
     <h1>{{ countStore.doubleCount }}</h1>
     <button @click="countStore.increment()">按钮2</button>
+    <button @click="addMore">按钮3</button>
     <hr>
     <button @click="getDog">再来一个</button>
     <br>
@@ -25,7 +26,7 @@
 <script lang="ts" setup name="Person">
 
 
-import { onBeforeMount, onMounted, withDefaults, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted,ref } from 'vue';
+import { onBeforeMount, onMounted, withDefaults, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted,ref,toRefs } from 'vue';
 import { type PersonInterface, type Persons } from '@/types/index';
 import useSum from '@/composition/useSum';
 import useDog from '@/composition/useDog';
@@ -36,9 +37,15 @@ const {dogList,getDog} = useDog();//最佳实践
 
 const n = ref(0);
 const countStore = useCounterStore();
+//我之前犯过的错误，我知道了，解构需要torefs，但是不推荐这么写，这个玩意好危险
+//const {count, doubleCount, increment} = toRefs(useCounterStore());
 console.log('@@',countStore);
 
-
+const addMore = () => {
+    countStore.$patch({//一次性修改所有属性，取决于你函数写在哪里，写在store里，就直接用那边的
+        count : 999
+    })
+}
 
 
 
