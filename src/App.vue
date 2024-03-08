48920333cfe6d228e5a04300249db6d6afb92dcc
Sean <fengying1028@hotmail.com>
2024-03-07 22:47:00 -0600

    sass

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+	<Test></Test>
 	<div class="outter">
 		<h2>sum: {{ sum }}</h2>
 		<br>
@@ -49,6 +50,7 @@ import { ref, reactive, shallowRef, shallowReactive, readonly, shallowReadonly, 
 import CustomRef from '@/components/CustomRef.vue';
 import Modal from '@/components/Modal.vue'
 import Child from '@/components/Child.vue'
+import Test from '@/components/Test.vue'
 import {myReactive} from '@/utils/myReactive'
 // shallowRef只能处理第一层数据，就是.value可以，再往下不行了
 //这个的用处是当我只关注这个对象是否被人替换的时候，用这个，而不关注里面的属性是否变化
---
+++ src/components/Test.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="bem-test">
+        abc
+        <div class="bem-test__inner">
+            test
+        </div>
+        <div class="bem-test--abc">test1</div>
+        <div class="bem-test--b">test2</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+@include b(test){
+    color: red;
+    font-size: 30px;
+    @include e(inner){
+        color: green;
+    }
+    @include m(abc){
+        color: blue;
+    }
+    @include m(b){
+        color: aqua;
+    }
+}
+
+
+</style>
